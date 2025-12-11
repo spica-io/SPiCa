54ece0c217a7ff3d90ac8dbe44a610f2aec9b259
YoonJiwon <gaden119000@gmail.com>
2025-12-11 22:38:50 +0900

    MultiGetHandler 구현

--- src/main/java/com/spica/handler/MultiGetHandler.java
+++ src/main/java/com/spica/handler/MultiGetHandler.java
@@ -12,6 +12,18 @@ public class MultiGetHandler {
     }
 
     void handle(final ChannelHandlerContext ctx, final String[] input) {
+        final String command = input[0];
+        final int keyCount = input.length - 1;
 
+        for (int i = 1; i <= keyCount; i++) {
+            final String key = input[i];
+            final String value = store.getOrDefault(key, null);
+
+            if (value == null) {
+                ctx.writeAndFlush("존재하지 않는 key: " + key + "\n");
+            } else {
+                ctx.writeAndFlush(value + "\n");
+            }
+        }
     }
 }
