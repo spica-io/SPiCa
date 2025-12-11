2abf50c231bf971ce35762e8f7501b0266137f61
YoonJiwon <gaden119000@gmail.com>
2025-12-11 21:43:55 +0900

    setIfMatches에서 ConcurrentHashMap.replace(key, oldValue, newValue) 사용

--- src/main/java/com/spica/handler/SetHandler.java
+++ src/main/java/com/spica/handler/SetHandler.java
@@ -28,23 +28,20 @@ public class SetHandler {
     }
 
     void setIfMatches(final ChannelHandlerContext ctx, final String[] input) {
-
         final String command = input[0];
         final String key = input[1];
         final String newValue = input[2];
         final String oldValue = input[4];
 
-        final String currentOldValue = store.getOrDefault(key, null);
-
-        if (currentOldValue == null) {
-            ctx.writeAndFlush("존재하지 않는 key: " + key + "\n");
-            return;
+        if (store.replace(key, oldValue, newValue)){
+            ctx.writeAndFlush("OK\n");
+        } else {
+            final String currentValue = store.getOrDefault(key, null);
+            if (currentValue == null) {
+                ctx.writeAndFlush("존재하지 않는 key: " + key + "\n");
+            } else {
+                ctx.writeAndFlush("올바르지 않은 이전 값: " + oldValue + "\n");
+            }
         }
-        if (!currentOldValue.equals(oldValue)) {
-            ctx.writeAndFlush("올바르지 않은 이전 값: " + oldValue + "\n");
-            return;
-        }
-        store.replace(key, newValue);
-        ctx.writeAndFlush("OK\n");
     }
 }
