b5103f4ed6a73c9d8416dd84775b9ea04f039f2d
YoonJiwon <gaden119000@gmail.com>
2025-12-11 22:16:41 +0900

    MultiGetHandler 추가

--- src/main/java/com/spica/handler/MultiGetHandler.java
+++ src/main/java/com/spica/handler/MultiGetHandler.java
@@ -1,4 +1,17 @@
 package com.spica.handler;
 
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Map;
+
 public class MultiGetHandler {
+    private final Map<String, String> store;
+
+    public MultiGetHandler(Map<String, String> store) {
+        this.store = store;
+    }
+
+    void handle(final ChannelHandlerContext ctx, final String[] input) {
+
+    }
 }
