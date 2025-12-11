357c8519704a806dc90e74646518e12ca25eaed1
ori0o0p <tmddnjsrla99@gmail.com>
2025-12-11 22:09:16 +0900

    Duration으로 변경

--- src/main/java/com/spica/handler/SleepHandler.java
+++ src/main/java/com/spica/handler/SleepHandler.java
@@ -1,9 +1,12 @@
 package com.spica.handler;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
 public class SleepHandler {
 
     public void handle(final String[] input) throws InterruptedException {
         final long time = Long.parseLong(input[1]);
-        Thread.sleep(time * 1000);
+        Thread.sleep(Duration.of(time, ChronoUnit.SECONDS));
     }
 }
